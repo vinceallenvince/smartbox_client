457abd0b23d2f0d99854d17dc68bb9d738e86d5f
Vince Allen <vince@spotify.com>
2014-10-09 21:36:39 -0400

    Updates.

--- smartbox_client.py
+++ smartbox_client.py
@@ -57,7 +57,7 @@ def scale(val, src, dst):
     return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
 
 def clear():
-    req = Request(base_client_uri + "/clear")
+    req = Request(base_server_uri + "/clear")
     urlopen(req)
 
 def thumbup():
